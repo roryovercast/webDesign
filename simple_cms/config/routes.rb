cda9fd0282fcea9b8e4b776f389fc078c865f209
Rory Overcast <rory.overcast@gmail.com>
2014-03-24 01:44:40 -0500

    Created Subjects and Sections. Using Layouts.

--- simple_cms/config/routes.rb
+++ simple_cms/config/routes.rb
@@ -1,7 +1,8 @@
 SimpleCms::Application.routes.draw do
-  root "demo#index"
+ 
+  root "pages#index"
   #get "demo/index"
-  match ':controller(/:action(/:id))', :via => :get
+  match ':controller(/:action(/:id))', :via => [:get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
